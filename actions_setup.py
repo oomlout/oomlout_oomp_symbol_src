e069626260ddedcff1523a439b3cddd30d771c8f
Oom Lout <aaron@oomlout.com>
2023-09-14 18:18:44 +0100

    added oomp_templates

--- actions_setup.py
+++ actions_setup.py
@@ -3,8 +3,8 @@ import oom_kicad
 
 def main():
     pass
-    oomlout_oomp_symbol_src.clone_and_copy_symbols(test=False)
-    oomlout_oomp_symbol_src.make_symbols_readme()
+    oomlout_oomp_symbol_src.clone_and_copy_symbols(test=True)
+    #oomlout_oomp_symbol_src.make_symbols_readme()
     oom_kicad.push_to_git()    
 
 
